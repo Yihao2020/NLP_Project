eb1f73b850d459521688933f21c9ee8bb3501183
TCT86 <81772650+TCT86@users.noreply.github.com>
2021-04-19 17:28:57 -0500

    Add files via upload

--- model_architectures/Naive_Bayes_Toxic_Comments.py
+++ model_architectures/Naive_Bayes_Toxic_Comments.py
@@ -36,6 +36,9 @@ def main():
     print()
 
     scores = []
+    recalls = []
+    precisions = []
+    F1s = []
 
     for i in range(length):
         print(features[i])
@@ -89,15 +92,64 @@ def main():
         print("confusion matrix:")
         print(cm)
         print()
+    # Recall
+        recall = metrics.recall_score(y_test, pred)
+        recalls.append(recall)
+        print("Recall Score:")
+        print(recall)
+        print()
+    #Precision
+        precision = metrics.precision_score(y_test, pred)
+        precisions.append(precision)
+        print("Precision Score:")
+        print(precision)
+        print()
+    #F1
+        F1 = metrics.f1_score(y_test, pred)
+        F1s.append(F1)
+        print("f1 Score:")
+        print(F1)
+        print()
+    #Final
+        print(metrics.classification_report(y_test, pred))
+        print()
 
     total = 0
     for s in scores:
-        x = s*100
-        total += x
+        total += s
+
     average_percent = total/len(scores)
-    print("Average accuracy score:")
+    print("Average Accuracy Score:")
     print(average_percent)
     print()
+
+    total_recall = 0
+    for r in recalls:
+        total_recall += r
+
+    average_recall = total_recall / len(recalls)
+    print("Average Recall Score:")
+    print(average_recall)
+    print()
+
+    total_pre = 0
+    for p in precisions:
+        total_pre += p
+
+    average_pre = total_pre / len(precisions)
+    print("Average Precision Score:")
+    print(average_pre)
+    print()
+
+    total_f1 = 0
+    for f in F1s:
+        total_f1 += f
+
+    average_f1 = total_f1 / len(F1s)
+    print("Average F1 Score:")
+    print(average_f1)
+    print()
+
     print("Done")
 
 if __name__ == "__main__":
