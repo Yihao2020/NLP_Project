7ba3157b449111e076b1dfd95c999421c385abc4
Devesh Kumar <devesh@tamu.edu>
2021-04-21 23:52:11 -0500

    Final commit

--- model_architectures/run_BiLSTM.py
+++ model_architectures/run_BiLSTM.py
@@ -1,10 +1,10 @@
 from  model_architectures import BiLSTM
 
 model = BiLSTM.BiLSTM1(epochs = 10)
-model.train("embedding.pkl")
+model.train("../data/embedding.pkl")
 model.evaluate()
 model.plot_metrics()
 model = BiLSTM.BiLSTM2(epochs = 10)
-model.train("embedding.pkl")
+model.train("../data/embedding.pkl")
 model.evaluate()
 model.plot_metrics()
