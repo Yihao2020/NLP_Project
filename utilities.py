e16f3cd455be623b12a7dd572aa8ef8c6ba01150
Devesh Kumar <deveshsharma1427@gmail.com>
2021-04-21 21:02:02 -0500

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,5 +1,9 @@
 
+
 import pandas as pd
+import logging
+import sys
+
 def load_data_csv_to_dataframe(file_path):
     data = pd.read_csv(file_path)
     return data
@@ -12,6 +16,12 @@ def save_data_dataframe_to_csv(data,filename = None):
         data.to_csv(index=False)
 
 
+def get_console_handler():
+   console_handler = logging.StreamHandler(sys.stdout)
+   console_handler.setFormatter(logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s"))
+   return console_handler
 
-def evaluate():
-    pass
+logger = logging.getLogger("NLP_PROEJCT")
+logger.setLevel(logging.INFO) # better to have too much log than not enough
+logger.addHandler(get_console_handler())
+logger.propagate = False
