a533172ba91123c5b3e318050429f20acac85d58
Devesh Kumar <devesh@tamu.edu>
2021-04-21 20:20:51 -0500

    Restructured

---
+++ data_preprocessing/sentence_bert_embedding.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+#from data_preprocessing.nltk_preprocessing import preprocess_file
+from utilities import logger, load_data_csv_to_dataframe
+
+
+def generate_bert_embeddings(filename):
+    model = SentenceTransformer('paraphrase-distilroberta-base-v1')
+    data = pd.read_pickle("../data/train_data_processed.pkl")
+
+    for index, row in data.iterrows():
+        text = row['comment_text']
+
+        emb = model.encode(text)
+        data.at[index, 'comment_text'] = emb
+
+    return data
+
+
+
+
+
+
